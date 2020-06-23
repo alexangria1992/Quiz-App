9e93c1bf2c45c0579507056f318cf0a3a4e56589
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-23 15:07:45 +1200

    Created Event Listener

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTrue = findViewById(R.id.btnTrue);
+        View.OnClickListener myClickListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                if(view.getId() == R.id.btnTrue){
+                    Log.i("MyApp", "btn true is tapped now!");
+                }
+            }
+        };
+        btnTrue.setOnClickListener(myClickListener);
+
+        Button btnWrong = findViewById(R.id.btnWrong);
+
+        btnWrong.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Log.i("MyApp", "btn Wrong is tapped now");
+            }
+        });
+
+
     }
 }
